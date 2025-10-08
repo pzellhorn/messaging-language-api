159a583a90528cbaf847e74b3ebcf0a8948ee97a
Peter Zellhorn - Personal <peterzellhorn@gmail.com>
2025-10-08 05:54:59 +0200

    item option dto

---
+++ FlutterMessaging.DTO/FlashCards/FlashCardSetItemOptionDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlutterMessaging.DTO.FlashCards
+{ 
+    public class FlashCardSetItemOptionDTO
+    {
+        public Guid FlashCardSetTemplateItemOptionId { get; set; } 
+        public Guid FlashCardSetTemplateItemId { get; set; } 
+        public string OptionKey { get; set; } = string.Empty; 
+        public string OptionValue { get; set; } = string.Empty;
+
+    }
+}
