01d24d73e0482c3f346b4a84f1749d8faa2b39fc
Peter Zellhorn - Personal <peterzellhorn@gmail.com>
2025-10-08 05:44:08 +0200

    flash card set type enum

---
+++ FlutterMessaging.DTO/Consts/FlashCardSetTemplateTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlutterMessaging.DTO.Consts
+{
+    public enum FlashCardSetTemplateTypes
+    {
+        None = 0,
+        QuestionAnswer = 1,
+    }
+}
