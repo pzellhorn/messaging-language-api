bdc535fbe47bbdb7c442f8d7b579b82b3204b192
Peter Zellhorn - Personal <peterzellhorn@gmail.com>
2025-10-16 18:01:59 +0200

    delete old DTOs

--- FlutterMessaging.DTO/FlashCards/FlashCardSetTemplateItemDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FlutterMessaging.DTO.FlashCards
-{
-    public class FlashCardSetTemplateItemDTO
-    {
-        public Guid FlashCardSetTemplateItemId { get; set; } 
-        public Guid FlashCardSetTemplateId { get; set; } 
-        public string Question { get; set; } = string.Empty;
-    }
-}
