267dbd20afafad1b309d08f59762968ce8a1e8cc
Peter Zellhorn - Personal <peterzellhorn@gmail.com>
2025-11-07 05:28:47 +0200

    run migration to add device installation table

---
+++ src/FlutterMessaging.State/Migrations/MessagingDb/20251107032639_messaging_notificationDeviceInstallation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlutterMessaging.State.Migrations.MessagingDb
+{
+    /// <inheritdoc />
+    public partial class messaging_notificationDeviceInstallation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "device_installations",
+                columns: table => new
+                {
+                    device_installation_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    profile_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    device_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    notification_push_token = table.Column<string>(type: "text", nullable: false),
+                    device_model = table.Column<string>(type: "text", nullable: true),
+                    time_zone = table.Column<string>(type: "text", nullable: true),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()"),
+                    modified_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_device_installations", x => x.device_installation_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "device_installations");
+        }
+    }
+}
--- src/FlutterMessaging.State/migration.md
+++ src/FlutterMessaging.State/migration.md
@@ -1,36 +1,4 @@
-
-# --Windows --
-# Run migration in root (/API/FlutterMessagingApi/src/) 
-
-$MigrationName     = "messaging_fixPrimaryKeyChatRoomMessage"
-
-$Project  = "FlutterMessaging.State"
-$Startup  = "FlutterMessaging.API"
-$Context  = "FlutterMessaging.State.Data.MessagingDbContext"
-$OutDir   = Join-Path $Project "Migrations"
-$OutFile  = Join-Path $OutDir "$MigrationName.sql"
-New-Item -ItemType Directory -Force -Path $OutDir | Out-Null
-
-dotnet ef migrations add $MigrationName `
-  --project $Project `
-  --startup-project $Startup `
-  --context $Context `
-&& dotnet ef migrations script --idempotent `
-  -o $OutFile `
-  --project $Project `
-  --startup-project $Startup `
-  --context $Context
-
-
-# --Windows --
-# apply to docker pod, still in the same root folder:  
-
-$composeDir = Resolve-Path (Join-Path $Project 'database')
-Push-Location $composeDir.Path
-$cid = docker compose ps -q db
-Get-Content -Raw (Join-Path ..\Migrations "$MigrationName.sql") | docker exec -i $cid psql -U peter -d messagingdb
-Pop-Location
-
+d 
  
 --unix--
 #docker exec -i $(docker compose ps -q db) psql -U peter -d messagingdb < ../Migrations/pluralize_tables.sql
